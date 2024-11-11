f057b72dd634642425384adb3ed470bcc543206e
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-11-10 22:57:41 -0500

    init kafka consumer

---
+++ src/main/java/api/azure/msv_kafka_consumer/MsvKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package api.azure.msv_kafka_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvKafkaConsumerApplication.class, args);
+	}
+
+}
